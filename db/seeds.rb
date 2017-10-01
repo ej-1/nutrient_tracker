982e5a9786b6b3ad5e89e03985c19a2e3a4c09f7
Erik <erikwjonsson@gmail.com>
2017-10-01 13:45:19 +0200

    Added some eaten_foods data to seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+eaten_foods = EatenFood.create([
+	{ name: 'Broccoli', amount: 300 },
+  { name: 'Kyckling bröstfilé u skinn ugnsstekt', amount: 300, eaten_at: '2017-05-01T00:00:00+00:00' },
+  { name: 'Ris långkornigt kokt m salt', amount: 200, eaten_at: '2017-05-01T00:00:00+00:00' },
+  { name: 'Havregrynsgröt fullkorn', amount: 60, eaten_at: '2017-05-01T00:00:00+00:00' },
+  { name: 'Mellanmjölk fett  1,5% berik m D-vitamin', amount: 400, eaten_at: '2017-05-01T00:00:00+00:00' },
+  { name: 'Blåbärssoppa', amount: 200, eaten_at: '2017-05-01T00:00:00+00:00' },
+])
+
+eaten_foods_2 = EatenFood.create([
+  { name: 'Broccoli', amount: 300, eaten_at: '2017-05-02T00:00:00+00:00' },
+  { name: 'Kyckling bröstfilé u skinn ugnsstekt', amount: 300, eaten_at: '2017-05-02T00:00:00+00:00' },
+  { name: 'Ris långkornigt kokt m salt', amount: 300, eaten_at: '2017-05-02T00:00:00+00:00' },
+  { name: 'Havregrynsgröt fullkorn', amount: 60, eaten_at: '2017-05-02T00:00:00+00:00' },
+  { name: 'Mellanmjölk fett  1,5% berik m D-vitamin', amount: 400, eaten_at: '2017-05-02T00:00:00+00:00' },
+  { name: 'Blåbärssoppa', amount: 200, eaten_at: '2017-05-02T00:00:00+00:00' },
+])
